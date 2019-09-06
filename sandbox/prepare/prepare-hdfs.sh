40fbfd26104a7b6dbe90fd0be765ff5632879f14
Andrei Bylinovich <andrei.bylinovich@gmail.com>
2019-09-06 08:11:48 +0300

    Archive files post-processing.

--- sandbox/prepare/prepare-hdfs.sh
+++ sandbox/prepare/prepare-hdfs.sh
@@ -4,7 +4,13 @@
 echo "Prepare HDFS..."
 /usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -mkdir /tmp/dataset
 /usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -mkdir /tmp/dataset/errors
+/usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -mkdir /tmp/dataset/archive
+/usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -mkdir /tmp/dataset/archive/hotels
+/usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -mkdir /tmp/dataset/archive/weather
+/usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -mkdir /tmp/dataset/archive/expedia
+
 /usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -copyFromLocal /tmp/sandbox/dataset/* hdfs://sandbox-hdp:8020/tmp/dataset
+
 /usr/hdp/current/hadoop-hdfs-client/bin/hdfs dfs -chmod -R 777 /tmp/dataset
 
 
